273e0b13f211110a213dc142a6282caad4e30b6c
Kanmii <maneptha@gmail.com>
2021-05-13 01:47:36 +0200

    all projects linting/type-checking should be per package

--- package-scripts.js
+++ package-scripts.js
@@ -6,12 +6,25 @@ const commonScripts = require("./_shared/_package-scripts");
 
 const packagesPath = resolvePath(__dirname, "packages");
 
-const packagesScripts = [
+const packages = [
   ["cra", "cra"],
   ["cy", "cy"],
   ["be", "be"],
   ["shared", "sh"],
-].reduce((acc, [packagePath, alias]) => {
+];
+
+function runPerPackage(commandFn, condFn = () => true) {
+  return packages.reduce((acc, [pkg, alias]) => {
+    const path = resolvePath(packagesPath, pkg);
+    if (condFn(path)) {
+      const command = commandFn(path, pkg, alias);
+      acc.push(command);
+    }
+    return acc;
+  }, []);
+}
+
+const packagesScripts = packages.reduce((acc, [packagePath, alias]) => {
   const script = resolvePath(packagesPath, packagePath, "package-scripts");
 
   if (existsSync(`${script}.js`)) {
@@ -34,6 +47,38 @@ module.exports = {
       script: `sort-package-json ./package.json \
           && sort-package-json ./packages/**/package.json`,
       description: `Sort package json`,
+    },
+    tc: {
+      script: runPerPackage(
+        (path, pkg) => {
+          return ` echo "type-checking package: ${pkg}" && \
+            cd ${path} && \
+            yarn start tc && \
+            cd ../..`;
+        },
+        (path) => {
+          const tsConfigPath = resolvePath(path, "tsconfig.json");
+          return existsSync(tsConfigPath);
+        }
+      ).join(" && "),
+
+      description: "type check project packages in turn",
+    },
+    lint: {
+      script: runPerPackage(
+        (path, pkg) => {
+          return ` echo "lint package: ${pkg}" && \
+            cd ${path} && \
+            yarn start lint && \
+            cd ../..`;
+        },
+        (path) => {
+          const tsConfigPath = resolvePath(path, "tsconfig.json");
+          return existsSync(tsConfigPath);
+        }
+      ).join(" && "),
+
+      description: "type check project packages in turn",
     },
   },
 };
